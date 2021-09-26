b71e2a7d710fd43cb1f91a5e8953f46083c4546b
kevinlee0701 <kevinlee0701@aliyun.com>
2021-09-26 14:15:07 +0800

    添加future

--- src/test/java/com/kevinlee/demo/Thread/bean/TestFuture.java
+++ src/test/java/com/kevinlee/demo/Thread/bean/TestFuture.java
@@ -2,27 +2,67 @@ package com.kevinlee.demo.Thread.bean;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class TestFuture {
 
     @Test
-    void test() throws ExecutionException, InterruptedException {
+    void testTaskFuture() throws ExecutionException, InterruptedException {
         // 创建任务T2的FutureTask
         FutureTask<String> ft2 = new FutureTask(new T2Task());
         // 创建任务T1的FutureTask
         FutureTask<String> ft1 = new FutureTask(new T1Task(ft2));
         // 线程T1执行任务ft1
-        Thread T1 = new Thread(ft1,"T1-Thread");
+        Thread T1 = new Thread(ft1, "T1-Thread");
         T1.start();
         // 线程T2执行任务ft2
-        Thread T2 = new Thread(ft2,"T2-Thread");
+        Thread T2 = new Thread(ft2, "T2-Thread");
         T2.start();
         // 等待线程T1执行结果
         System.out.println(ft1.get());
+    }
+    @Test
+    void testCompletableFuture() {
+        //任务1：洗水壶->烧开水
+        CompletableFuture<Void> f1 =
+                CompletableFuture.runAsync(() -> {
+
+                    System.out.println("T1:洗水壶...");
+                    sleep(1, TimeUnit.SECONDS);
+
+                    System.out.println("T1:烧开水...");
+                    sleep(15, TimeUnit.SECONDS);
+                });
+
+        //任务2：洗茶壶->洗茶杯->拿茶叶
+        CompletableFuture<String> f2 =
+                CompletableFuture.supplyAsync(() -> {
+                    System.out.println("T2:洗茶壶...");
+                    sleep(1, TimeUnit.SECONDS);
+
+                    System.out.println("T2:洗茶杯...");
+                    sleep(2, TimeUnit.SECONDS);
+
+                    System.out.println("T2:拿茶叶...");
+                    sleep(1, TimeUnit.SECONDS);
+                    return "龙井";
+                });
+        //任务3：任务1和任务2完成后执行：泡茶
+        CompletableFuture<String> f3 =
+                f1.thenCombine(f2, (__, tf) -> {
+                    System.out.println("T1:拿到茶叶:" + tf);
+                    System.out.println("T1:泡茶...");
+                    return "上茶:" + tf;
+                });
+        //等待任务3执行结果
+        System.out.println(f3.join());
+    }
+
+    void sleep ( int t, TimeUnit u){
+        try {
+            u.sleep(t);
+        } catch (InterruptedException e) {
+        }
     }
 
     // T1Task需要执行的任务：
@@ -34,6 +74,7 @@ public class TestFuture {
         T1Task(FutureTask<String> ft2) {
             this.ft2 = ft2;
         }
+
         @Override
         public String call() throws Exception {
             System.out.println("T1:洗水壶...");
