0cd871266f032f70766edaefdbb593b107c838d1
kevinlee0701 <kevinlee0701@aliyun.com>
2021-09-28 18:20:02 +0800

    添加线程池

--- src/test/java/com/kevinlee/demo/Thread/TestSpringBoot.java
+++ src/test/java/com/kevinlee/demo/Thread/TestSpringBoot.java
@@ -2,13 +2,14 @@ package com.kevinlee.demo.Thread;
 
 import com.kevinlee.demo.DemoApplication;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.context.junit4.SpringRunner;
-
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DemoApplication.class)
 public class TestSpringBoot {
@@ -17,6 +18,11 @@ public class TestSpringBoot {
 
     @Test
     public void test(){
-        System.out.println(threadPoolTaskExecutor);
+        threadPoolTaskExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                log.info("线程id={},name={}",Thread.currentThread().getId(),Thread.currentThread().getName());
+            }
+        });
     }
 }
