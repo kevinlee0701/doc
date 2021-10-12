44087ad8f4a585c578a7397221c842d82b94ef66
kevinlee0701 <kevinlee0701@aliyun.com>
2021-10-12 17:35:43 +0800

    添加注释

---
+++ src/test/java/com/kevinlee/demo/Thread/ReadWriteLockTest.java
@@ -0,0 +1,48 @@
+package com.kevinlee.demo.Thread;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 读写互斥，写写互斥，读读不互斥
+ * 把读写操作分别用读锁和写锁来加锁，在读取时，多个线程可以同时获得读锁，这样就大大提高了并发读的执行效率。
+ *
+ * 使用ReadWriteLock时，适用条件是同一个数据，有大量线程读取，但仅有少数线程修改。
+ *
+ * 例如，一个论坛的帖子，回复可以看做写入操作，它是不频繁的，但是，浏览可以看做读取操作，是非常频繁的，这种情况就可以使用ReadWriteLock。
+ *
+ * 使用ReadWriteLock可以提高读取效率：
+ *
+ * ReadWriteLock只允许一个线程写入；
+ *
+ * ReadWriteLock允许多个线程在没有写入时同时读取；
+ *
+ * ReadWriteLock适合读多写少的场景。
+ *
+ */
+public class ReadWriteLockTest {
+    private final ReadWriteLock rwlock = new ReentrantReadWriteLock();
+    private final Lock rlock = rwlock.readLock();
+    private final Lock wlock = rwlock.writeLock();
+    private int[] counts = new int[10];
+
+    public void inc(int index) {
+        wlock.lock(); // 加写锁
+        try {
+            counts[index] += 1;
+        } finally {
+            wlock.unlock(); // 释放写锁
+        }
+    }
+
+    public int[] get() {
+        rlock.lock(); // 加读锁
+        try {
+            return Arrays.copyOf(counts, counts.length);
+        } finally {
+            rlock.unlock(); // 释放读锁
+        }
+    }
+}
---
+++ src/test/java/com/kevinlee/demo/Thread/ReentrantLockAndConditionTest.java
@@ -0,0 +1,56 @@
+package com.kevinlee.demo.Thread;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 使用ReentrantLock比直接使用synchronized更安全，可以替代synchronized进行线程同步。
+ *
+ * 但是，synchronized可以配合wait和notify实现线程在条件不满足时等待，条件满足时唤醒，用ReentrantLock我们怎么编写wait和notify的功能呢？
+ *
+ * 答案是使用Condition对象来实现wait和notify的功能。
+ *
+ * 使用Condition时，引用的Condition对象必须从Lock实例的newCondition()返回，这样才能获得一个绑定了Lock实例的Condition实例。
+ *
+ * Condition提供的await()、signal()、signalAll()原理和synchronized锁对象的wait()、notify()、notifyAll()是一致的，并且其行为也是一样的：
+ *
+ * await()会释放当前锁，进入等待状态；
+ *
+ * signal()会唤醒某个等待线程；
+ *
+ * signalAll()会唤醒所有等待线程；
+ *
+ * 唤醒线程从await()返回后需要重新获得锁。
+ *
+ */
+public class ReentrantLockAndConditionTest {
+
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+    private Queue<String> queue = new LinkedList<>();
+
+    public void addTask(String s) {
+        lock.lock();
+        try {
+            queue.add(s);
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public String getTask() throws InterruptedException {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                condition.await();
+            }
+            return queue.remove();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
