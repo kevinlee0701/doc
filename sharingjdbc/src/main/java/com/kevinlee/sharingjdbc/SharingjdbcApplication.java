2762cd5732ba8c1808555dfeb9060ba2aa74aabf
kevinlee0701 <kevinlee0701@aliyun.com>
2021-11-26 14:49:21 +0800

    sharingjdbc

---
+++ sharingjdbc/src/main/java/com/kevinlee/sharingjdbc/SharingjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.kevinlee.sharingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingjdbcApplication.class, args);
+    }
+
+}
