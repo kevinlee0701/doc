a34bddc4353a84c4e2cc1034399eca45db43400e
kevinlee0701 <kevinlee0701@aliyun.com>
2022-01-11 20:52:22 +0800

    book - war

--- book/src/main/java/com/kevinlee/book/BookApplication.java
+++ book/src/main/java/com/kevinlee/book/BookApplication.java
@@ -2,10 +2,11 @@ package com.kevinlee.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.kevinlee"})
 @SpringBootApplication
-public class BookApplication {
+public class BookApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
