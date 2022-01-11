517f468209e0e954061e1c30178eb2ddf3845d57
kevinlee0701 <kevinlee0701@aliyun.com>
2022-01-11 20:15:33 +0800

    book - war

--- book/src/main/java/com/kevinlee/book/BookController.java
+++ book/src/main/java/com/kevinlee/book/BookController.java
@@ -1,17 +1,19 @@
 package com.kevinlee.book;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
-
+@Slf4j
 @Controller
 public class BookController {
 
 
     @RequestMapping("/")
     public String index(){
+        log.info("进入首页============");
         return "index";
     }
 
