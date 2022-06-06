79bb55a2e45e93d099a4b9d333701f261d34e63a
kevinlee0701 <kevinlee0701@aliyun.com>
2022-06-06 15:30:11 +0800

    添加es

---
+++ elasticsearch/src/test/java/com/kevinlele/elasticsearch/EsTest.java
@@ -0,0 +1,58 @@
+package com.kevinlele.elasticsearch;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.Iterator;
+
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EsTest {
+
+    @Resource
+    private RestHighLevelClient restHighLevelClient;
+
+    String index ="logstash-log";
+    String testId="49273";
+
+    /**
+     * 判断索引是否存在
+     *
+     * @throws IOException
+     */
+    @Test
+    public void test() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices(index);
+
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.query(QueryBuilders.matchAllQuery());
+        builder.size(20);
+
+        searchRequest.source(builder);
+        SearchResponse response = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+        Iterator<SearchHit> iterator = hits.iterator();
+        while (iterator.hasNext()) {
+            log.info("输出数据:" + iterator.next().getSourceAsString());
+        }
+
+    }
+
+}
