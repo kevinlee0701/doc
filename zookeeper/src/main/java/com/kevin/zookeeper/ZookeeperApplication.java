93f166d47f3740b9c168123f126e120537fbc082
kevinlee <kevilee0701@aliyun.com>
2022-09-14 16:27:28 +0800

    zookeeper

---
+++ zookeeper/src/main/java/com/kevin/zookeeper/ZookeeperApplication.java
@@ -0,0 +1,15 @@
+package com.kevin.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collection;
+
+@SpringBootApplication
+public class ZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperApplication.class, args);
+    }
+
+}
