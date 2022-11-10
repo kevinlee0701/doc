5aba775b777514d3f0ec4402f60bc4750ad9721b
kevinlee <kevilee0701@aliyun.com>
2022-11-10 14:45:47 +0800

    zookeeper

---
+++ nio/src/main/java/com/kevinlee/nio/Test.java
@@ -0,0 +1,19 @@
+package com.kevinlee.nio;
+
+import java.util.stream.Collectors;
+
+/**
+ * 类 描 述：TODO
+ * 创建时间：2022/10/13 18:25
+ * 创 建 人：lifeng
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        test(1);
+    }
+
+    public static  void test(int ... test){
+        System.out.println("1");
+    }
+}
