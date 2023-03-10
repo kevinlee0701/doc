6d93b62e473682f56257091ec3faddb2e0311d83
kevinlee <kevilee0701@aliyun.com>
2023-03-10 17:26:52 +0800

    尝试apifox

--- web-app/src/main/java/com/kevinlee/webapp/WebController.java
+++ web-app/src/main/java/com/kevinlee/webapp/WebController.java
@@ -1,7 +1,15 @@
 package com.kevinlee.webapp;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName WebController
@@ -12,10 +20,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
     @RequestMapping("/test")
-    public String test(){
-        if(1==1){
-            throw new RuntimeException("WebController is error");
-        }
-        return "index";
+    @ResponseBody
+    public String test(@RequestBody JSONArray array){
+        List list = JSONObject.parseArray(array.toJSONString());
+        return JSON.toJSONString(list);
     }
 }
