991990ac6b03af19d2c454b7b7cbdc1bdef0c83d
kevinlee <kevilee0701@aliyun.com>
2023-06-08 18:19:28 +0800

    时间交叉合并

--- juc/src/main/java/com/kevin/juc/dateConversion/DateTimeConversion.java
+++ juc/src/main/java/com/kevin/juc/dateConversion/DateTimeConversion.java
@@ -1,23 +1,27 @@
 package com.kevin.juc.dateConversion;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateTimeConversion {
     public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         // 将 LocalDateTime 转换为 Date
         LocalDateTime localDateTime = LocalDateTime.now();
         Date date = Date.from(localDateTime.atZone(ZoneId.of("Asia/Shanghai")).toInstant());
 
-        System.out.println("LocalDateTime: " + localDateTime);
-        System.out.println("Date: " + date);
+        System.out.println("LocalDateTime: " + localDateTime.format(formatter));
+        System.out.println("Date: " +sdf.format(date));
 
         // 将 Date 转换为 LocalDateTime
         Date currentDate = new Date();
         LocalDateTime convertedDateTime = LocalDateTime.ofInstant(currentDate.toInstant(), ZoneId.of("Asia/Shanghai"));
 
-        System.out.println("Date: " + currentDate);
-        System.out.println("LocalDateTime: " + convertedDateTime);
+        System.out.println("Date: " +sdf.format(currentDate) );
+        System.out.println("LocalDateTime: " + convertedDateTime.format(formatter));
     }
 }
