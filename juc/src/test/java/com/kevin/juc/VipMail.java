4fc60741e270886747749848fd55d9d94d6f8d6a
kevinlee <kevilee0701@aliyun.com>
2023-12-21 09:57:32 +0800

    web应用

--- juc/src/test/java/com/kevin/juc/VipMail.java
+++ juc/src/test/java/com/kevin/juc/VipMail.java
@@ -22,22 +22,25 @@ public class VipMail {
     @Test
     public void addFaJiaRenForProductLine() throws IOException {
       int[] productLines ={1,2,3,4,5};
-      int business_type=1;
-      int mailId=7;//数据库对应的邮箱
+      int business_type=1;//发送场景：1:排课邮件提醒,2:新vip规划报告提醒,5:教师学员第一次上课,6:小班课排课提醒;
+      int[] mailIds={7,8};//数据库对应的邮箱
       int mailType=1;//1，发送者；2，抄送人,3收件人
         List<String> sqlList = new ArrayList<>();
         for (int productLine : productLines) {
-            StringBuilder sql = new StringBuilder();
-            sql.append("insert into  pr_product_line_mail (business_type, product_line, mail_id, mail_type, create_time, update_time, is_delete)  values(")
-                    .append(business_type).append(",")
-                    .append(productLine).append(",")
-                    .append(mailId).append(",")
-                    .append(mailType).append(",")
-                    .append("now()").append(",")
-                    .append("now()").append(",")
-                    .append("0)").append(";");
-            sqlList.add(sql.toString());
-            System.out.println(sql.toString());
+            for (int mailId : mailIds) {
+                StringBuilder sql = new StringBuilder();
+                sql.append("insert into  pr_product_line_mail (business_type, product_line, mail_id, mail_type, create_time, update_time, is_delete)  values(")
+                        .append(business_type).append(",")
+                        .append(productLine).append(",")
+                        .append(mailId).append(",")
+                        .append(mailType).append(",")
+                        .append("now()").append(",")
+                        .append("now()").append(",")
+                        .append("0)").append(";");
+                sqlList.add(sql.toString());
+                System.out.println(sql.toString());
+            }
+
         }
     }
 }
