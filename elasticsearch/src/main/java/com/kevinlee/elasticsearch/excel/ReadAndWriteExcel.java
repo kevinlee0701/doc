1ec6d0dd0a37a91bdad231aa418bcdd4a79a8b2a
kevinlee <kevilee0701@aliyun.com>
2024-04-26 17:03:02 +0800

    抓数据

--- elasticsearch/src/main/java/com/kevinlee/elasticsearch/excel/ReadAndWriteExcel.java
+++ elasticsearch/src/main/java/com/kevinlee/elasticsearch/excel/ReadAndWriteExcel.java
@@ -1,8 +1,8 @@
 package com.kevinlee.elasticsearch.excel;
 
-import com.koolearn.qti.ExtensionManager;
-import com.koolearn.qti.SimpleQtiFacade;
-import com.koolearn.qti.custom.KooExtensionPackage;
+//import com.koolearn.qti.ExtensionManager;
+//import com.koolearn.qti.SimpleQtiFacade;
+//import com.koolearn.qti.custom.KooExtensionPackage;
 import com.koolearn.qti.delivery.SimpleDeliveryFacade;
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.hssf.usermodel.HSSFCell;
@@ -75,47 +75,47 @@ public class ReadAndWriteExcel {
      * 对同一个文件进行，读，写操作
      * @throws IOException
      */
-    public static void readW() throws IOException {
-        KooExtensionPackage kooExtensionPackage = new KooExtensionPackage();
-        ExtensionManager extensionManager = new ExtensionManager(kooExtensionPackage);
-        com.koolearn.qti.SimpleQtiFacade simpleQtiFacade = new SimpleQtiFacade(extensionManager);
-        com.koolearn.qti.delivery.SimpleDeliveryFacade simpleDeliveryFacade = new SimpleDeliveryFacade(simpleQtiFacade);
-        //读-文件
-        File readFile=new File("/Users/kevinlee/Desktop/ceshi.xlsx");
-        HSSFWorkbook readWorkbook=new HSSFWorkbook(FileUtils.openInputStream(readFile));
-        HSSFSheet readSheet=readWorkbook.getSheetAt(0);
-        int lastRowNum=readSheet.getLastRowNum();
-        for (int i=1;i<=lastRowNum;i++){
-            HSSFRow readRow=readSheet.getRow(i);
-            //获取当前行最后单元格列号
-            //int lastCellNum=readRow.getLastCellNum();
-            //读 -指定的几列--我需要2、3列 然后输出到6、7列
-            HSSFCell r2=readRow.getCell(2);
-            //读出来的数据 进行MD5加密
-            //根据需要自己处理数据
-            String value2=r2.getStringCellValue();
- 
-            //当前文件创建 一列 输出到 6、7列
-            HSSFCell cell3 = readRow.createCell(3);
-            //写入文件
-            String json = simpleDeliveryFacade.unmarshall(value2, 123);
-            cell3.setCellValue(json);
- 
-        }
-        FileOutputStream stream = FileUtils.openOutputStream(readFile);
-        readWorkbook.write(stream);
-        stream.close();
- 
-    }
- 
- 
+//    public static void readW() throws IOException {
+//        KooExtensionPackage kooExtensionPackage = new KooExtensionPackage();
+//        ExtensionManager extensionManager = new ExtensionManager(kooExtensionPackage);
+//        com.koolearn.qti.SimpleQtiFacade simpleQtiFacade = new SimpleQtiFacade(extensionManager);
+//        com.koolearn.qti.delivery.SimpleDeliveryFacade simpleDeliveryFacade = new SimpleDeliveryFacade(simpleQtiFacade);
+//        //读-文件
+//        File readFile=new File("/Users/kevinlee/Desktop/ceshi.xlsx");
+//        HSSFWorkbook readWorkbook=new HSSFWorkbook(FileUtils.openInputStream(readFile));
+//        HSSFSheet readSheet=readWorkbook.getSheetAt(0);
+//        int lastRowNum=readSheet.getLastRowNum();
+//        for (int i=1;i<=lastRowNum;i++){
+//            HSSFRow readRow=readSheet.getRow(i);
+//            //获取当前行最后单元格列号
+//            //int lastCellNum=readRow.getLastCellNum();
+//            //读 -指定的几列--我需要2、3列 然后输出到6、7列
+//            HSSFCell r2=readRow.getCell(2);
+//            //读出来的数据 进行MD5加密
+//            //根据需要自己处理数据
+//            String value2=r2.getStringCellValue();
+//
+//            //当前文件创建 一列 输出到 6、7列
+//            HSSFCell cell3 = readRow.createCell(3);
+//            //写入文件
+//            String json = simpleDeliveryFacade.unmarshall(value2, 123);
+//            cell3.setCellValue(json);
+//
+//        }
+//        FileOutputStream stream = FileUtils.openOutputStream(readFile);
+//        readWorkbook.write(stream);
+//        stream.close();
+//
+//    }
  
  
  
  
-    public static void main(String[] args) throws IOException {
-        readW();
-    }
+ 
+ 
+//    public static void main(String[] args) throws IOException {
+//        readW();
+//    }
  
  
 }
