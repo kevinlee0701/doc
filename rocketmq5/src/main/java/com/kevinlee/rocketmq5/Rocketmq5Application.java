540b0d2292ee833d4f08b7c6b973541b2de2d70c
kevinlee <kevilee0701@aliyun.com>
2024-06-04 14:52:22 +0800

    rocketmq5

---
+++ rocketmq5/src/main/java/com/kevinlee/rocketmq5/Rocketmq5Application.java
@@ -0,0 +1,13 @@
+package com.kevinlee.rocketmq5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rocketmq5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rocketmq5Application.class, args);
+    }
+
+}
---
+++ rocketmq5/src/test/java/com/kevinlee/rocketmq5/Rocketmq5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinlee.rocketmq5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rocketmq5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
