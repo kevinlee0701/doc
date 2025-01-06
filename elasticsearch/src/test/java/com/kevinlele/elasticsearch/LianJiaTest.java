4d96569ee99da2041712c9f248321ea8ee1b2503
kevinlee <kevilee0701@aliyun.com>
2025-01-06 09:55:56 +0800

    新加已读章节

--- elasticsearch/src/test/java/com/kevinlele/elasticsearch/LianJiaTest.java
+++ elasticsearch/src/test/java/com/kevinlele/elasticsearch/LianJiaTest.java
@@ -87,8 +87,8 @@ public class LianJiaTest {
 
     @Test
     public void beijing() throws Exception {
-        List<Court> bjCourts = Arrays.asList(Court.LONG_TENG_YUAN,Court.YUN_QU_YUAN,Court.LONG_HUA_YUAN);
-//        List<Court> bjCourts = Arrays.asList(Court.XIN_LONG_CHENG,Court.LONG_HUA_YUAN,Court.LONG_TENG_YUAN);
+//        List<Court> bjCourts = Arrays.asList(Court.LONG_TENG_YUAN,Court.YUN_QU_YUAN,Court.LONG_HUA_YUAN);
+        List<Court> bjCourts = Arrays.asList(Court.XIN_LONG_CHENG,Court.LONG_HUA_YUAN,Court.LONG_TENG_YUAN);
         String city = "bj";
         for (Court bjCourt : bjCourts) {
             boolean flag = lianjia(bjCourt.getCourt(), city);
